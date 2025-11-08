561d5c9f668ecb13fb9aed2ffc09791a85766f81
ArthurVeraci <arthurveraci5@gmail.com>
2025-11-08 09:17:39 -0300

    fix: corrigir mensagem esperada no teste automatizado

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,5 @@ from app import app
 def test_home_route():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello, DevOps!" in response.data
+    assert b"Hello, DevOps Lab!" in response.data
+
